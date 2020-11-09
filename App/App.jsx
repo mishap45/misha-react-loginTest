d6883db0028d3ff83adc8c4f47cf1b98c6180ea3
mishap45 <pavlovmisha45@gmail.com>
2020-11-09 13:50:15 +0200

    MiLov

---
+++ App/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AppNavigator from './screens/AppNavigator'
+import { NavigationContainer } from '@react-navigation/native'
+
+const App = () => {
+  return <NavigationContainer><AppNavigator/></NavigationContainer>
+};
+
+export default App
